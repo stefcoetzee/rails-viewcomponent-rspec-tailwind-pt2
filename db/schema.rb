f1de8aa49bc56bb9b97a90d3f6970a14b60261d4
Stef Coetzee <stefcoetzee@protonmail.com>
2022-03-06 07:20:48 +0200

    Add icon components

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_05_110651) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_02_05_110651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,16 +19,16 @@ ActiveRecord::Schema.define(version: 2022_02_05_110651) do
     t.string "last_name"
     t.string "position"
     t.integer "jersey_number"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "team_id", null: false
     t.index ["team_id"], name: "index_athletes_on_team_id"
   end
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "athletes", "teams"
